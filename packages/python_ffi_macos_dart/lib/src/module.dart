1df27393b567140e5e5b6ba05ba3f0c844c392d5
Julius Bredemeyer <48645716+IVLIVS-III@users.noreply.github.com>
2023-02-17 17:13:15 +0100

    Removed unnecessary _modules map from PythonModuleMacos.

--- packages/python_ffi_macos_dart/lib/src/module.dart
+++ packages/python_ffi_macos_dart/lib/src/module.dart
@@ -9,8 +9,6 @@ class PythonModuleMacos
           finalizer: _PythonObjectMacosRefcountUtil.finalizer,
         );
 
-  final Map<String, PythonModuleMacos> _modules = <String, PythonModuleMacos>{};
-
   _PythonClassDefinitionMacos _getClassDefinition(String name) {
     final _PythonObjectMacos classAttribute = getAttributeRaw(name);
     return _PythonClassDefinitionMacos(platform, classAttribute.reference);
@@ -31,16 +29,7 @@ class PythonModuleMacos
 
   @override
   PythonModuleMacos getModule(String name) {
-    final PythonModuleMacos? cachedModule = _modules[name];
-    if (cachedModule != null) {
-      return cachedModule;
-    }
-
     final _PythonObjectMacos moduleAttribute = getAttributeRaw(name);
-    final PythonModuleMacos module =
-        PythonModuleMacos(platform, moduleAttribute.reference);
-
-    _modules[name] = module;
-    return module;
+    return PythonModuleMacos(platform, moduleAttribute.reference);
   }
 }
