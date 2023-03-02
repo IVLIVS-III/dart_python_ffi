49b02a749710a8c9baa3853d3e1a96fa08dd3c73
Julius Bredemeyer <48645716+IVLIVS-III@users.noreply.github.com>
2023-03-02 18:06:17 +0100

    Continued reasearching the future protocol.

--- python_research/await.py
+++ python_research/await.py
@@ -1,7 +1,7 @@
 import asyncio
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 from datetime import datetime
-from typing import Any, Awaitable, Generator, Generic, Self, TypeVar
+from typing import Any, Awaitable, Generator, Generic, Optional, Self, TypeVar
 import time
 
 
@@ -40,7 +40,8 @@ class FutureWrapper(asyncio.Future):
             raise
 
     def _make_cancelled_error(self: Self):
-        print(f"├── calling @{now()} _make_cancelled_error() on {self}[@{id(self)}]")
+        print(
+            f"├── calling @{now()} _make_cancelled_error() on {self}[@{id(self)}]")
         try:
             result = self.future._make_cancelled_error()
             print(f"│   └── returns {result}")
@@ -60,7 +61,8 @@ class FutureWrapper(asyncio.Future):
             raise
 
     def __schedule_callbacks(self: Self):
-        print(f"├── calling @{now()} __schedule_callbacks() on {self}[@{id(self)}]")
+        print(
+            f"├── calling @{now()} __schedule_callbacks() on {self}[@{id(self)}]")
         try:
             result = self.future.__schedule_callbacks()
             print(f"│   └── returns {result}")
@@ -110,7 +112,8 @@ class FutureWrapper(asyncio.Future):
             raise
 
     def add_done_callback(self: Self, fn, *args, context=None):
-        print(f"├── calling @{now()} add_done_callback(fn={fn}, *args={args}, context={context}) on {self}[@{id(self)}]")
+        print(
+            f"├── calling @{now()} add_done_callback(fn={fn}, *args={args}, context={context}) on {self}[@{id(self)}]")
         try:
             result = self.future.add_done_callback(fn, *args, context=context)
             print(f"│   └── returns {result}")
@@ -120,7 +123,8 @@ class FutureWrapper(asyncio.Future):
             raise
 
     def remove_done_callback(self: Self, fn):
-        print(f"├── calling @{now()} remove_done_callback(fn={fn}) on {self}[@{id(self)}]")
+        print(
+            f"├── calling @{now()} remove_done_callback(fn={fn}) on {self}[@{id(self)}]")
         try:
             result = self.future.remove_done_callback(fn)
             print(f"│   └── returns {result}")
@@ -130,7 +134,8 @@ class FutureWrapper(asyncio.Future):
             raise
 
     def set_result(self: Self, result):
-        print(f"├── calling @{now()} set_result(result={result}) on {self}[@{id(self)}]")
+        print(
+            f"├── calling @{now()} set_result(result={result}) on {self}[@{id(self)}]")
         try:
             result = self.future.set_result(result)
             print(f"│   └── returns {result}")
@@ -140,7 +145,8 @@ class FutureWrapper(asyncio.Future):
             raise
 
     def set_exception(self: Self, exception):
-        print(f"├── calling @{now()} set_exception(exception={exception}) on {self}[@{id(self)}]")
+        print(
+            f"├── calling @{now()} set_exception(exception={exception}) on {self}[@{id(self)}]")
         try:
             result = self.future.set_exception(exception)
             print(f"│   └── returns {result}")
@@ -175,15 +181,39 @@ class FutureWrapper(asyncio.Future):
 @dataclass
 class GeneratorWrapper(Generic[T, T_Send, T_Return], Generator[T, T_Send, T_Return]):
     generator: Generator[T, T_Send, T_Return]
+    __simulated_yield_froms: list[Generator[T, T_Send, T_Return]] = field(
+        init=False, default_factory=list)
+
+    def __simulate_yield_from_next(self: Self) -> tuple[bool, T]:
+        while len(self.__simulated_yield_froms) > 0:
+            try:
+                return True, self.__simulated_yield_froms[-1].__next__()
+            except StopIteration:
+                self.__simulated_yield_froms = self.__simulated_yield_froms[:-1]
+        return False, None
 
     def __next__(self: Self) -> T:
         print(f"├── calling @{now()} __next__() on {self}[@{id(self)}]")
         try:
+            # simulate any outstanding yield from first
+            s, r = self.__simulate_yield_from_next()
+            if s:
+                return r
+
+            # run own next
             result = self.generator.__next__()
             if isinstance(result, asyncio.Future):
                 print("│   ├── converts result to FutureWrapper")
                 result = FutureWrapper(result)
-            print(f"│   ├── returns {result}")
+                print(
+                    f"│   ├── needs to simulate 'yield from' on that FutureWrapper, returning a GeneratorWrapper(FutureWrapper)")
+                self.__simulated_yield_froms.append(
+                    GeneratorWrapper(result.__iter__()))
+                s, r = self.__simulate_yield_from_next()
+                if s:
+                    return r
+
+            print(f"│   └── returns {result}")
             # print(f"│   └── with dir() = {dir(result)}")
             return result
         except BaseException as e:
@@ -201,7 +231,8 @@ class GeneratorWrapper(Generic[T, T_Send, T_Return], Generator[T, T_Send, T_Retu
             raise
 
     def send(self: Self, value: T_Send) -> T:
-        print(f"├── calling @{now()} send(value={value}) on {self}[@{id(self)}]")
+        print(
+            f"├── calling @{now()} send(value={value}) on {self}[@{id(self)}]")
         try:
             result = self.generator.send(value)
             print(f"│   └── returns {result}")
@@ -211,7 +242,8 @@ class GeneratorWrapper(Generic[T, T_Send, T_Return], Generator[T, T_Send, T_Retu
             raise
 
     def throw(self: Self, typ, val=None, tb=None) -> T:
-        print(f"├── calling @{now()} throw(typ={typ}, val={val}, tb={tb}) on {self}[@{id(self)}]")
+        print(
+            f"├── calling @{now()} throw(typ={typ}, val={val}, tb={tb}) on {self}[@{id(self)}]")
         try:
             result = self.generator.throw(typ)
             print(f"│   └── returns {result}")
@@ -254,7 +286,63 @@ async def my_coroutine(awaitable: Awaitable) -> int:
     result = await wrapped_awaitable
     return result
 
-if __name__ == "__main__":
-    print("starting asyncio.run")
+
+@dataclass
+class MySleep:
+    seconds: float
+    __start: float = field(init=False, default=time.time())
+    __count: int = field(init=False, default=0)
+
+    def __await__(self: Self) -> Generator[float, None, None]:
+        print(f"called __await__ {self.__count} times already @{now()}")
+        self.__count += 1
+        diff = time.time() - self.__start
+        if diff < self.seconds:
+            yield self
+        return diff
+
+
+def main1():
+    print("starting asyncio.run on main1")
     asyncio.run(my_coroutine(my_awaitable()))
     print("└── completed asyncion.run")
+
+
+async def amain2(awaitable: Awaitable[float]):
+    result = await awaitable
+    print(f"Awaited result: {result}")
+
+
+def main2():
+    print("Starting asyncio.run on main2")
+    asyncio.run(amain2(MySleep(1)))
+    print("└── completed asyncion.run")
+
+
+def main3():
+    ma1 = my_awaitable()
+    print(id(ma1))
+    ma2 = ma1.__await__()
+    print(id(ma2))
+    ma3 = ma2.__iter__()
+    print(id(ma3))
+
+    loop = asyncio.events.new_event_loop()
+    asyncio.events.set_event_loop(loop)
+    asyncio.events._set_running_loop(loop)
+    """with asyncio.Runner(debug=True) as runner:
+        runner.run(main)
+        runner.get_loop()"""
+    ma4 = ma3.__next__()
+    print(id(ma4), type(ma4))
+    print(dir(ma4))
+    ma5 = ma4.get_loop()
+    print(id(ma5), type(ma5))
+    print(dir(ma5))
+
+
+if __name__ == "__main__":
+    main1()
+    # main2()
+    # main3()
+    pass
