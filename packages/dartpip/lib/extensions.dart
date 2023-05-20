7985c37be3289f45026f790dd5ab90ed7b516c85
Julius Bredemeyer <48645716+IVLIVS-III@users.noreply.github.com>
2023-05-20 16:17:02 +0200

    Updated format commands.

--- packages/dartpip/lib/extensions.dart
+++ packages/dartpip/lib/extensions.dart
@@ -1,15 +1,11 @@
 part of dartpip;
 
 extension _KeyMappingExtension<K, V> on Map<K, V> {
-  Map<T, V> mapKeys<T>(
-    T Function(K key) f,
-  ) =>
+  Map<T, V> mapKeys<T>(T Function(K key) f) =>
       map((K key, V value) => MapEntry<T, V>(f(key), value));
 }
 
 extension _ValueMappingExtension<K, V> on Map<K, V> {
-  Map<K, T> mapValues<T>(
-    T Function(V value) f,
-  ) =>
+  Map<K, T> mapValues<T>(T Function(V value) f) =>
       map((K key, V value) => MapEntry<K, T>(key, f(value)));
 }
